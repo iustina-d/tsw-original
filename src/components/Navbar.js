488d55c4449f8d2bf5600f50dc42f24a3294715d
iustina-d <iustina2dulea@gmail.com>
2023-02-15 03:02:58 +0200

    clean code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import "./Navbar.css"
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fontwesome/react-fontawesome"
 import {
   faCircleInfo,
   faBars,
